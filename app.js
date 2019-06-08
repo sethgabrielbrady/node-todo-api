d3b16fdfebe5318185851a31b195089d801df209
Seth Brady <sethgabrielbrady@gmail.com>
2019-06-08 18:14:50 -0400

    added updated route to api

--- app.js
+++ app.js
@@ -9,17 +9,27 @@ router.get('/', (req, res) => res.send('Hello World!'));
 
 router.route('/todos')
   .get((req, res) => {
-     Todo.all(function(data){
-       console.log("Retrieving all Todos");
-       res.json(data);
-     });
+    Todo.all(function(data){
+      console.log('Retrieving all Todos');
+      res.json(data);
+    });
+
   })
   .post((req, res) => {
     Todo.createDocument(req.body, function(data){
-      console.log(`Todo created with id ${data._id}`);
+      // console.log(`Todo created with id ${data._id}`);
       res.json(data);
     });
   });
+
+router.route('/todos/:todo_id')
+  .put((req, res) => {
+    Todo.updateDocument(req.params.todo_id, req.body, function(data){
+      console.log(`Todo id ${data._id} updated!`);
+      res.json(data);
+    });
+  });
+
 
 app.use('/', router);
 
--- todo.js
+++ todo.js
@@ -13,26 +13,36 @@ db.once('open', function() {
   console.log('Connected to mongodb!');
 });
 
-
 const todoSchema = new mongoose.Schema({
   text: String,
   done: Boolean
 });
 
 todoSchema.statics.createDocument = function(params, callback){
-  return Todo.create({ text: params.text, done: params.done},
-    function (err, todo){
+  return Todo.create({ text: params.text, done: params.done },
+    function (err, todo) {
       if (err) return handleError(err);
       callback(todo);
     }
-  );
-};
+  )
+}
+
+todoSchema.statics.updateDocument = function(id, params, callback){
+  return Todo.findOneAndUpdate({ _id: id },
+                               {
+                                  text: params.text,
+                                  done: params.done
+                               }, function(err, todo){
+                                  callback(todo);
+                               })
+}
 
 todoSchema.statics.all = function(callback){
   return Todo.find({}, function(err, todos){
     callback({ todos: todos });
   });
 };
+
 const Todo = mongoose.model('Todo', todoSchema);
 
 module.exports = Todo;
